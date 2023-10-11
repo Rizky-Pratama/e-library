658ed7b8c1dc7404d625b6a2268f6dbf5a63cd1a
Muhammad Rizky Pratama <73880696+Rizky-Pratama@users.noreply.github.com>
2023-10-11 23:04:25 +0700

    Revert "Clean code navbar"

--- app/components/Navbar/ListItem.jsx
+++ app/components/Navbar/ListItem.jsx
@@ -32,31 +32,18 @@ const listRoutes = [
   },
 ];
 
-const isMobile = (device = "mobile") => {
-  if (device === "mobile") {
-    return "md:hidden";
-  } else if (device === "desktop") {
-    return "max-md:hidden";
-  }
-};
-
-const ListItems = ({ device }) => {
+const ListItem = () => {
   const pathname = usePathname();
 
   return (
-    <ul
-      className={
-        "py-4 flex gap-4 items-center text-base font-normal flex-nowrap overflow-x-auto " +
-        isMobile(device)
-      }
-    >
+    <ul className="py-4 flex gap-4 items-center text-base font-normal flex-nowrap overflow-x-auto md:hidden">
       {listRoutes.map((data, i) => (
         <li
           key={i}
           className={
             pathname == data.path
-              ? "border-b-2 border-slate-950 dark:border-slate-200 -translate-y-1 transition-all cursor-pointer"
-              : "hover:-translate-y-1 transition-all cursor-pointer"
+              ? "border-b-2 border-slate-900 dark:border-slate-200 -translate-y-1 transition-all"
+              : "hover:-translate-y-1 transition-all"
           }
         >
           <Link
@@ -72,12 +59,31 @@ const ListItems = ({ device }) => {
   );
 };
 
-const ListItem = () => {
-  return <ListItems/>;
-};
-
 export const ListItemsDeks = () => {
-  return <ListItems device={"desktop"} />;
+  const pathname = usePathname();
+
+  return (
+    <ul className="py-4 flex gap-4 items-center text-base font-normal flex-nowrap overflow-x-auto max-md:hidden">
+      {listRoutes.map((data, i) => (
+        <li
+          key={i}
+          className={
+            pathname == data.path
+              ? "border-b-2 border-slate-900 dark:border-slate-200 -translate-y-1 transition-all cursor-pointer"
+              : "hover:-translate-y-1 transition-all cursor-pointer"
+          }
+        >
+          <Link
+            href={data.path}
+            className="flex gap-1 justify-center items-center"
+          >
+            {data.icon}
+            <span>{data.title}</span>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ListItem;
--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import ListItem, { ListItemsDeks } from "./ListItem";
 import Avatar from "../Avatar/Avatar";
 const Navbar = () => {
   return (
-    <nav className="bg-slate-50 text-slate-950 rounded-b dark:bg-slate-800 dark:text-slate-200 shadow-sm dark:shadow-slate-700">
+    <nav className="bg-slate-50 text-slate-950 rounded-b dark:bg-slate-800 dark:text-slate-200 shadow-sm">
       <div className="px-2 pt-2 mx-auto container flex-col md:py-2">
         <div className="flex justify-between items-center">
           <span className="text-xl lg:text-2xl font-semibold dark:text-white">E-library</span>
