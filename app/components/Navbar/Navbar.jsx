5d6c0517004533c176e5e71e99f787a61641e86f
Muhammad Rizky Pratama <73880696+Rizky-Pratama@users.noreply.github.com>
2023-10-11 15:50:32 +0000

    Add cards

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="bg-slate-50 text-slate-950 rounded-b dark:bg-slate-800 dark:text-slate-200 shadow-sm dark:shadow-slate-700">
       <div className="px-2 pt-2 mx-auto container flex-col md:py-2">
         <div className="flex justify-between items-center">
-          <span className="text-xl lg:text-2xl font-semibold dark:text-white">E-library</span>
+          <span className="text-lg md:text-xl lg:text-2xl font-semibold dark:text-white">E-library</span>
           <ListItemsDeks />
           <div className="flex items-center gap-2">
             <Toggle />
