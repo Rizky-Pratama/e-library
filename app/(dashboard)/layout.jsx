0bc9a85513f4a1bd478e043e13e1b4821747ee17
Rizky-pratama <hnaura89@gmail.com>
2024-03-03 14:00:26 +0700

    Fix import typo in Dashboard layout

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,4 +1,4 @@
-import DashboardLayout from "@/components/layout/Dashboardlayout";
+import DashboardLayout from "@/components/layout/DashboardLayout";
 
 export default function Layout({ children }) {
   return (
